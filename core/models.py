e223821f1b8f760e9e1f3fe16abb9ff47ee46bab
Gabriel Fernandes <gabrielnobrega63@gmail.com>
2018-06-21 18:45:09 -0300

    editaveis prontos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class mvv(models.Model):
+    missao = models.TextField()
+    visao = models.TextField()
+    valores = models.TextField()
--- core/views.py
+++ core/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def index (request):
+    
+    mvv_var = mvv.objects.get()
+    
+    context = {
+        'visao': mvv_var.visao,
+        'valores': mvv_var.valores,
+        'missao': mvv_var.missao,
+    }
+    
     return render(
         request, 
-        'index.html',
+        'index.html', context
     )
