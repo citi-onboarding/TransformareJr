71563941621820a8e23cbfce0f6efdaffa450329
lucasmorais6 <lma6@cin.ufpe.br>
2018-06-21 19:15:27 -0300

    contato back e front atualizado

--- core/views.py
+++ core/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from templated_email import send_templated_mail
 
 # Create your views here.
 def index (request):
+
+    if request.method == 'POST':
+        nome=request.POST.get('nome')
+        email=request.POST.get('email')
+        telefone=request.POST.get('telefone')
+        conheceu=request.POST.get('conheceu')
+        assunto=request.POST.get('assunto')
+        mensagem=request.POST.get('mensagem')
+        send_templated_mail(
+        template_name='email',
+        from_email= email,
+        recipient_list=['contato.transformarejr@gmail.com'],
+        context={
+            'nome':nome,
+            'email':email,
+            'telefone':telefone,
+            'conheceu':conheceu,
+            'assunto':assunto,
+            'mensagem':mensagem
+        },)
+
     return render(
         request, 
         'index.html',
