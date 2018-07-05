ae8340c2f7e3b0f43c7dcb00c5fd79b873e3d074
eulalia <memgra@cin.ufpe.br>
2018-07-05 15:12:04 -0300

    tirando senha da empresa

--- transformarejr/settings.py
+++ transformarejr/settings.py
@@ -103,6 +103,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'contato.transformarejr@gmail.com'
-EMAIL_HOST_PASSWORD = 'squadgabriel123@'
+EMAIL_HOST_PASSWORD = ''
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
